1899902ab4ab73d67f7ec4d3741c9f62ed359d10
David Young <davidsamuelyoung@protonmail.com>
2026-02-10 09:19:49 +0200

    test

--- AnalysisChain/MatlabProcServ.m
+++ AnalysisChain/MatlabProcServ.m
@@ -12,27 +12,25 @@ hdf5_input = 1;
 InputRCFFilename = '';
 InputHDF5Filename = '';
 
-CPI_s = 0.5; % Integration time (0.5s is standard for FM)
+CPI_s = 4.0; % Increased from 0.5s to 4.0s for Integration Gain
 EnableCancellation = 1;
 
-% The actual distance between Tx and Rx from your XML
+% Geometry
 TxToReferenceRxDistance_m = 74483;
 
-% Cancellation Range: Must be GREATER than TxToReferenceRxDistance_m.
-% This defines how far past the direct signal we want to cancel clutter.
-% 74483 + 3000m buffer = 77500
-CancellationMaxRange_m = 77500;
+% Cancellation: Cancel Direct signal + ~3.5km of clutter
+CancellationMaxRange_m = 78000;
 
-% ARD Range: Must be large enough to see the target.
-% Target is at ~10km altitude, moving.
-% Let's look out to 120km bistatic range to be safe.
-ARDMaxRange_m = 120000;
+% ARD Range: Target moves between 95km and 142km.
+% We set Max to 150km to ensure we capture the start of the track.
+ARDMaxRange_m = 150000;
 
-CancallationMaxDoppler_Hz = 200; % Increased to catch the moving target
-CancellationNInterations = 10;
+% Doppler: Target expected at 87Hz, window set to 200Hz
+CancallationMaxDoppler_Hz = 200;
+CancellationNInterations = 30; % Increased from 10 to 30
 CancellationNSegments = 8;
 
-ARDMaxDoppler_Hz = 300;
+ARDMaxDoppler_Hz = 200;
 outputARDPath = './Output';
 
 if hdf5_input == 1
@@ -42,46 +40,44 @@ if hdf5_input == 1
     RefFile = 'ArmasuisseRefRxClean.h5';
     SurvFile = 'ArmasuisseSurRxClean.h5';
 
-    % FERS outputs data in chunks. For a single run, it is usually chunk_000000.
-    % We read I and Q separately and combine them.
-
     % Load Reference Data
     try
-        [I_ref, Q_ref, scale_ref] = loadfersHDF5(refFile);
-        refData = complex(I_ref, Q_ref) * scale_ref;
+        % Note: loadfersHDF5 must be in ./ARDMakers/
+        [I_ref, Q_ref, scale_ref] = loadfersHDF5(RefFile);
+        RefData = complex(I_ref, Q_ref) * scale_ref;
     catch
-        error('Could not read Reference HDF5. Check filename or chunk name.');
+        error('Could not read Reference HDF5. Check filename or path.');
     end
 
     % Load Surveillance Data
     try
-        [I_sur, Q_sur, scale_sur] = loadfersHDF5(surFile);
-        surData = complex(I_sur, Q_sur) * scale_sur;
+        [I_sur, Q_sur, scale_sur] = loadfersHDF5(SurvFile);
+        SurvData = complex(I_sur, Q_sur) * scale_sur;
     catch
-        error('Could not read Surveillance HDF5. Check filename or chunk name.');
+        error('Could not read Surveillance HDF5. Check filename or path.');
     end
 
     fprintf('HDF5 file read completed\n');
 
-    % Hardcode parameters from your SingleTargetClean.fersxml
-    % The script expects uHz (microHertz), so we multiply Hz by 1e6
-    SR_uHz = 204800 * 1e6;   % Sample Rate from XML
-    GCF_uHz = 89e6 * 1e6;    % Carrier Frequency from XML
-    GCBW_uHz = 100e3 * 1e6;  % Bandwidth (Approx for FM)
+    % Hardcode parameters from SingleTargetClean.fersxml
+    SR_uHz = 204800 * 1e6;
+    GCF_uHz = 89e6 * 1e6;
+    GCBW_uHz = 100e3 * 1e6;
 
-    % Compute the number of samples in the CPI
-    % (SR_uHz / 1e6) converts back to Hz for calculation
+    % Compute samples per CPI
     CPISize_nSamp = floor((double(SR_uHz) / 1000000) * CPI_s);
-    TotalNumSamples = min(length(RefData), length(SurvData));
 
-    % Ensure data lengths match
+    % Ensure data lengths match and truncate to multiple of CPI
+    minLen = min(length(RefData), length(SurvData));
+    TotalNumSamples = floor(minLen / CPISize_nSamp) * CPISize_nSamp;
+
     RefData = RefData(1:TotalNumSamples);
     SurvData = SurvData(1:TotalNumSamples);
 else
     oInputRCFHeader = cRCF;
     oInputRCFHeader.readHeaderFromFile(InputRCFFilename);
 
-    CPISize_nSamp = oInputRCFHeader.getFs_Hz() * 4;
+    CPISize_nSamp = oInputRCFHeader.getFs_Hz() * CPI_s;
     TotalNumSamples = oInputRCFHeader.getNSamples();
 end
 
@@ -92,50 +88,51 @@ CGLSAlpha = 0;
 
 for CPINo = 0:nCPIs - 1
 
-fprintf('Processing CPI %i of %i:\n', CPINo + 1, nCPIs);
+    fprintf('Processing CPI %i of %i:\n', CPINo + 1, nCPIs);
 
-%Read data from the RCF or construct a dummy RCF object from the parsed HDF5 data
-if hdf5_input == 1
+    % Create RCF Object for this CPI
     oCPIRCF = cRCF;
-    oCPIRCF.m_fvReferenceData = RefData(CPIStartSampleNumber + 1 : CPIStartSampleNumber + CPISize_nSamp);
-    oCPIRCF.m_fvSurveillanceData = SurvData(CPIStartSampleNumber + 1 : CPIStartSampleNumber + CPISize_nSamp);
-    oCPIRCF.m_NSamples = CPISize_nSamp;
-    oCPIRCF.m_Fs_Hz = double(SR_uHz) / 1000000;
-    oCPIRCF.m_Fc_Hz = (GCF_uHz(1)) / 1000000;
-    oCPIRCF.m_Bw_Hz = double(GCBW_uHz) / 1000000;
-    oCPIRCF.m_TimeStamp_us = CPIStartSampleNumber * 1000000 / oCPIRCF.m_Fs_Hz;
-else
-    oCPIRCF = oInputRCFHeader.readFromFile(InputRCFFilename, CPIStartSampleNumber+1, CPISize_nSamp);
-end
+    if hdf5_input == 1
+        idxStart = CPIStartSampleNumber + 1;
+        idxEnd = CPIStartSampleNumber + CPISize_nSamp;
 
-%Now advance starting sample by 1 CPI for next interation
-CPIStartSampleNumber = CPIStartSampleNumber + CPISize_nSamp;
+        oCPIRCF.m_fvReferenceData = RefData(idxStart : idxEnd);
+        oCPIRCF.m_fvSurveillanceData = SurvData(idxStart : idxEnd);
+        oCPIRCF.m_NSamples = CPISize_nSamp;
+        oCPIRCF.m_Fs_Hz = double(SR_uHz) / 1000000;
+        oCPIRCF.m_Fc_Hz = (GCF_uHz(1)) / 1000000;
+        oCPIRCF.m_Bw_Hz = double(GCBW_uHz) / 1000000;
+        oCPIRCF.m_TimeStamp_us = CPIStartSampleNumber * 1000000 / oCPIRCF.m_Fs_Hz;
+    else
+        oCPIRCF = oInputRCFHeader.readFromFile(InputRCFFilename, CPIStartSampleNumber+1, CPISize_nSamp);
+    end
 
-if(EnableCancellation)
-    fprintf('Doing cancellation\n');
+    % Advance sample counter
+    CPIStartSampleNumber = CPIStartSampleNumber + CPISize_nSamp;
+
+    if(EnableCancellation)
+        fprintf('Doing cancellation (Iter: %d)...\n', CancellationNInterations);
+        tic
+        [oCPIRCF, CGLSAlpha] = CGLS_Cancellation(oCPIRCF, CancellationMaxRange_m, CancallationMaxDoppler_Hz, TxToReferenceRxDistance_m, CancellationNSegments, CancellationNInterations, CGLSAlpha);
+        toc
+    end
+
+    fprintf('Doing range/Doppler processing (FX)...\n');
     tic
-    [oCPIRCF, CGLSAlpha] = CGLS_Cancellation(oCPIRCF, CancellationMaxRange_m, CancallationMaxDoppler_Hz, TxToReferenceRxDistance_m, CancellationNSegments, CancellationNInterations, CGLSAlpha);
-    %oCPIRCF = ECA_Cancellation(oCPIRCF, CancellationMaxRange_m, CancallationMaxDoppler_Hz, TxToReferenceRxDistance_m, CancellationNSegments);
+
+    oARD = FX_ARD(oCPIRCF, ARDMaxRange_m, ARDMaxDoppler_Hz, TxToReferenceRxDistance_m);
     toc
-end
 
-fprintf('Doing range/Doppler processing\n');
-tic
-%oARD = Batches_ARD(oCPIRCF, ARDMaxRange_m, ARDMaxDoppler_Hz, TxToReferenceRxDistance_m);
-oARD = FX_ARD(oCPIRCF, ARDMaxRange_m, ARDMaxDoppler_Hz, TxToReferenceRxDistance_m);
-toc
+    % Write ARD to file
+    ARDFilename = outputARDPath;
+    if(ARDFilename(length(ARDFilename)) ~= '/')
+        ARDFilename = [ARDFilename '/'];
+    end
 
-%Write ARD to file:
-ARDFilename = outputARDPath;
-if(ARDFilename(length(ARDFilename)) ~= '/')
-    ARDFilename = [ARDFilename '/'];
-end
+    ARDFilename = [ARDFilename num2str(CPINo), '.ard'];
 
-% ARDFilename = [ARDFilename oARD.timeStampToString(), '.ard'];
-ARDFilename = [ARDFilename num2str(CPINo), '.ard'];
+    oARD.writeToFile(ARDFilename);
 
-oARD.writeToFile(ARDFilename);
-
-fprintf('\n\n');
+    fprintf('\n');
 
 end
--- AnalysisChain/ReadARDs.m
+++ AnalysisChain/ReadARDs.m
@@ -5,8 +5,12 @@ addpath('./Classes')
 clear; clc; close all;
 
 % User defined
-plot_all = 0; % Plot 3D function 1/0
-ARD_File = 'Output/100.ard';
+% Since CPI is 4s, try looking at the middle of the run
+ARD_File = 'Output_archive/1970-01-01T02.02.28.000000.ard';
+
+if ~exist(ARD_File, 'file')
+    error('File not found. Run MatlabProcServ first.');
+end
 
 % Plot 2D function
 figure(1);
@@ -14,9 +18,13 @@ oARD2 = cARD;
 oARD2.readFromFile(ARD_File);
 
 % Plot using Meters and Hz
-% The last two arguments are Max/Min dB for the color scale.
-% You might need to tweak -40 to -60 or -20 depending on signal strength.
-oARD2.plot2D('m','Hz', 0, -50);
+% Max Amplitude: 0 dB (Target)
+% Min Amplitude: -40 dB (Noise Floor)
+fprintf('Plotting %s with dynamic range -40dB to 0dB\n', ARD_File);
+oARD2.plot2D('m','Hz', 0, -40);
+
+% Add a marker for the baseline (Direct Signal)
+xline(74483, 'r--', 'Direct Path');
 
 % Plot 3D function
 %if plot_all == 1
