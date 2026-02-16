681fc1c9ce06e10dd7cceda3506453fd96f2cbcd
David Young <davidsamuelyoung@protonmail.com>
2026-02-16 13:18:08 +0200

    some images

--- AnalysisChain/ReadARDs.m
+++ AnalysisChain/ReadARDs.m
@@ -12,9 +12,18 @@ clear; clc; close all;
 %ARD_File = 'Ref_Outputs/Latest_FIXED_CleanSingleTarget_no_rand/16.ard';
 %ARD_File = 'Ref_Outputs/Latest_FIXED_JamSingleTarget_tone_stationary_jam/16.ard';
 %ARD_File = 'Ref_Outputs/CleanSingleTarget/1970-01-01T02.01.00.000000.ard';
-ARD_File = 'Ref_Outputs/JamSingleTarget_oldest_no_rand/16.ard';
-ARD_File = 'Ref_Outputs/JamSingleTarget_oldest_no_rand_stationary/16.ard';
+%ARD_File = 'Ref_Outputs/JamSingleTarget_oldest_no_rand/16.ard';
+%ARD_File = 'Ref_Outputs/JamSingleTarget_oldest_no_rand_stationary/16.ard';
 %ARD_File = 'Output/7.ard';
+
+ARD_File = 'Ref_Outputs/Latest_FIXED_CleanSingleTarget_no_rand/16.ard';
+%ARD_File = 'Ref_Outputs/Latest_FIXED_CleanSingleTarget_fers_latest/16.ard';
+%ARD_File = 'Ref_Outputs/Latest_FIXED_JamSingleTarget_no_rand/16.ard';
+%ARD_File = 'Ref_Outputs/Latest_FIXED_JamSingleTarget_fers_latest/16.ard';
+%ARD_File = 'Ref_Outputs/Latest_FIXED_JamSingleTarget_stationary_jam/16.ard';
+%ARD_File = 'Ref_Outputs/Latest_FIXED_JamSingleTarget_fers_latest_low_power_1mw/16.ard';
+%ARD_File = 'Ref_Outputs/Latest_FIXED_JamSingleTarget_low_power_1mw/16.ard';
+%ARD_File = 'Ref_Outputs/Latest_FIXED_JamSingleTarget_low_power_100uw/16.ard';
 
 % something funky happens at ard index 38 or 152s
 
@@ -23,4 +32,4 @@ figure(1);
 oARD2 = cARD;
 oARD2.readFromFile(ARD_File);
 % Can use (km or m) (m/s or Hz)
-oARD2.plot2D('m', 'Hz', 0, -40);
+oARD2.plot2D('m', 'Hz', 0, -10);
